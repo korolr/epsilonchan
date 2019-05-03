9b106060a1fcc998f8910b833345151d86821ee6
wizard <wizardsbureau@protonmail.com>
2019-05-03 22:01:01 +0300

    Need two rework backend container and the way backend runs localy

--- main.py
+++
@@ -1,5 +0,0 @@
-from server import server
-
-
-if __name__ == '__main__':
-    server.run_backend()
--- server/main.py
+++ server/main.py
@@ -2,7 +2,7 @@ import sys
 
 from aiohttp import web
 from aiohttp_graphql import GraphQLView
-from server.schema import schema
+from schema import schema
 
 app = web.Application()
 GraphQLView.attach(app, schema=schema, graphiql=True)
@@ -12,9 +12,5 @@ if sys.platform != 'win32':
     uvloop.install()
 
 
-def run_backend():
-    web.run_app(app, host='127.0.0.1', port=7000)
-
-
 if __name__ == '__main__':
-    run_backend()
+    web.run_app(app, host='127.0.0.1', port=7000)
