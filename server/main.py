6d29be7cd98d9c154e6b52de630203fa0f7fa4af
Alexsey Ramzaev <ekstazu@gmail.com>
2019-05-04 23:16:34 +0400

    add logger, async programming in python is awesome!

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
 import sys
 from aiohttp import web
 from aiohttp_graphql import GraphQLView
+import logging
+
 if sys.platform == 'win32':
     from server.schema import schema
 else:
@@ -10,7 +12,13 @@ if sys.platform != 'win32':
     import uvloop
     uvloop.install()
 
-app = web.Application()
+stdio_handler = logging.StreamHandler()
+stdio_handler.setLevel(logging.INFO)
+_logger = logging.getLogger('aiohttp.access')
+_logger.addHandler(stdio_handler)
+_logger.setLevel(logging.DEBUG)
+
+app = web.Application(logger=_logger)
 GraphQLView.attach(app, schema=schema, graphiql=True)
 
 if __name__ == '__main__':
