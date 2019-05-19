caed8afc6c9b3d266b9f5397ef79b58f9d1c11cd
wizard <wizardsbureau@protonmail.com>
2019-05-19 14:30:30 +0300

    fix imports

--- server/main.py
+++ server/main.py
@@ -3,10 +3,7 @@ from aiohttp import web
 from aiohttp_graphql import GraphQLView
 import logging
 
-if sys.platform == 'win32':
-    from server.schema import schema
-else:
-    from schema import schema
+import schema
 
 if sys.platform != 'win32':
     import uvloop
@@ -19,7 +16,7 @@ _logger.addHandler(stdio_handler)
 _logger.setLevel(logging.DEBUG)
 
 app = web.Application(logger=_logger)
-GraphQLView.attach(app, schema=schema, graphiql=True)
+GraphQLView.attach(app, schema=schema.schema, graphiql=True)
 
 if __name__ == '__main__':
     web.run_app(app, port=7000)
